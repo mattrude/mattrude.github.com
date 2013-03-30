3809392f24c0b8d47b2b2d435852e9e00002c138
Matt Rude <matt@mattrude.com>
2013-03-30 01:55:49 -0500

    Website Update

--- build-github-pages.sh
+++ build-github-pages.sh
@@ -61,9 +61,44 @@ do
     markdown readme.md >> index.html && \
     cat ../../comments.txt >> index.html
     sed "s/###SOURCE###/$project/g" ../../footer.txt >> index.html
-    rm -f style.css && cp ../../style.css .
+    rm -f style.css 
+    #cp ../../style.css .
 	rm -f favicon.ico
     rm -f readme.md
+
+
+        if [ -d git-wiki ]; then
+            rm -rf wiki
+            mkdir -p $DIR/projects/$project/wiki
+            echo "<?php ?>" > $DIR/projects/$project/wiki/index.html
+            cd git-wiki
+            for WIKIMD in `ls *.md`
+            do
+                echo "Working on page $WIKIMD"
+                WIKI=`echo $WIKIMD |sed 's/\.md//g'`
+                LDIR="$DIR/projects/$project/wiki/$WIKI"
+                rm -rf $LDIR
+                mkdir -p $LDIR/
+                cat $DIR/header.txt > $LDIR/index.html
+                PAGENAME=`echo $WIKI |sed 's/-/ /g'`
+                sed "s/###TITLE###/$PAGENAME/g" $DIR/header.txt |sed "s/###SOURCE###/$project/g" > $LDIR/index.html && \
+                echo "<div id=\"title\"><h1>$PAGENAME <i> &mdash; </i> gh.mattrude.com/$project</h1></div>" >> $LDIR/index.html && \
+                echo "<div id=\"breadcrums\"><p><a href=\"/\">gh.mattrude.com</a> / <a href=\"/$project\">$project</a> / wiki / <strong>$PAGENAME</strong></p></div>" >> $LDIR/index.html && \
+                markdown $WIKIMD >> $LDIR/index.html
+                cat $DIR/footer.txt >> $LDIR/index.html
+                sed -i "s/src=\"images/src=\"https:\/\/github.com\/mattrude\/$project\/wiki\/images/g" $LDIR/index.html
+            done
+            cd $DIR/projects/$project
+            rm -rf git-wiki
+            mv index.html index.html-work && cat index.html-work |sed "s/https:\/\/github.com\/mattrude\/$project\/wiki\//http:\/\/gh.mattrude.com\/$project\/wiki\//g" > index.html && rm -f index.html-work
+
+            git add .
+            git commit . -m "Adding/updating the Wiki pages to $project"
+            git push
+        fi
+
+
+
     REPOSTATUS=`git status -s |wc -l`
     if [ $REPOSTATUS != "0" ]; then
         git add .
@@ -84,6 +119,6 @@ markdown index.md >> index.html && \
 sed "s/###SOURCE###/mattrude.github.com/g" footer.txt >> index.html
 REPOSTATUS=`git status -s |wc -l`
 if [ $REPOSTATUS != "0" ]; then
-    git commit index.html -m "Website Update"
+    git commit . -m "Website Update"
     git push --all
 fi
